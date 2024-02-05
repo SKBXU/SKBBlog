0fe39fe2fab54938662adbcfa6f3b44d9ab09c21
SKBXU <skbxu@outlook.com>
2024-02-05 19:14:48 +0800

    博客系统

---
+++ skbblog-blog/src/main/java/com/skbblog/SKBBlogApplication.java
@@ -0,0 +1,13 @@
+package com.skbblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SKBBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SKBBlogApplication.class, args);
+    }
+
+}
