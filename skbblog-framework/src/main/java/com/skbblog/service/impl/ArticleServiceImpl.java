0e708d9d2141e5d5cddf24e1c23a938b5c5f633c
SKBXU <skbxu@outlook.com>
2024-03-16 16:16:49 +0800

    文章分类

--- skbblog-framework/src/main/java/com/skbblog/service/impl/ArticleServiceImpl.java
+++ skbblog-framework/src/main/java/com/skbblog/service/impl/ArticleServiceImpl.java
@@ -4,29 +4,41 @@ package com.skbblog.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.skbblog.constants.SystemConstants;
 import com.skbblog.domain.ResponseResult;
 import com.skbblog.domain.entity.Article;
+import com.skbblog.domain.vo.HotArticleVo;
 import com.skbblog.mapper.ArticleMapper;
 import com.skbblog.service.ArticleService;
+import com.skbblog.utils.BeanCopyUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> implements ArticleService {
     @Override
     public ResponseResult hotArticleList() {
-        //查询热门文章，封装成ResponseResult返回
+        //查询热门文章，封装成ResponseResult返回，使用MybatisPlus的LambdaQueryWrapper方法
         LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<>();
         //必须是正式文章
-        queryWrapper.eq(Article::getStatus,0);
+        queryWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
         //按照浏览量进行排序
         queryWrapper.orderByDesc(Article::getViewCount);
         //最多只查询10条
         Page<Article> page = new Page(1,10);
         page(page,queryWrapper);
-
         List<Article> articles = page.getRecords();
-        return ResponseResult.okResult(articles);
+        //bean，实体类和Vo之间的拷贝
+//        List<HotArticleVo> articleVos = new ArrayList<>();
+//        for (Article article : articles) {
+//            HotArticleVo vo = new HotArticleVo();
+//            BeanUtils.copyProperties(article,vo);
+//            articleVos.add(vo);
+//        }
+        List<HotArticleVo> vs = BeanCopyUtils.copyBeanList(articles,HotArticleVo.class);
+        return ResponseResult.okResult(vs);
     }
 }
